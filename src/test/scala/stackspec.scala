f87ead6f65cc694587ac7b5fb431fbb11bed8aa5
Jessica Kerr <jessitron@gmail.com>
2013-06-12 21:29:55 -0500

    I couldn't stand it. I changed the sample test to use an immutable stack. This change let me confirm that guard works for me here.

--- src/test/scala/stackspec.scala
+++ src/test/scala/stackspec.scala
@@ -1,19 +1,20 @@
-import collection.mutable.Stack
+import collection.immutable.Stack
 import org.scalatest._
+import org.scalatest.matchers.ShouldMatchers
 
 class StackSpec extends FlatSpec with ShouldMatchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
     val stack = new Stack[Int]
-    stack.push(1)
-    stack.push(2)
-    stack.pop() should equal (2)
-    stack.pop() should equal (1)
+    val stackOf2 = stack.push(1).push(2)
+    val (first, stackOf1) = stackOf2.pop2
+    first should equal (2)
+    stackOf1.pop2._1 should equal (1)
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
     val emptyStack = new Stack[String]
-    evaluating { emptyStack.pop() } should produce [NoSuchElementException]
+    evaluating { emptyStack.pop } should produce [NoSuchElementException]
   }
 }
 
