9e106e038b8655d3e9d5370a5fe7274cdd488fc0
Mario Aquino <mario.e.aquino@gmail.com>
2013-06-17 08:55:00 -0500

    Experimental implementation. Kinda icky.

--- src/main/scala/fib.scala
+++ src/main/scala/fib.scala
@@ -1,6 +1,23 @@
 object Mathy {
-  def fib(n:Int): Int = {
-    if (n == 0) 0
-    else 1
+  def fib(n: Int): Int = {
+    predictBasedOnPrevious2(0, 1, _ + _).take(n + 1).last
+  }
+
+  // a way to map over a stream that gets the previous two elements
+  def predictBasedOnPrevious2( firstSeed: Int, secondSeed: Int, f: (Int, Int) => Int): Iterable[Int] = {
+    new Iterable[Int] {
+      def iterator: Iterator[Int] = new Iterator[Int] {
+        var nextResult = firstSeed
+        var nextNextResult = secondSeed
+
+        def hasNext = true
+        def next():Int = {
+          val tempResult = nextResult
+          nextResult = nextNextResult
+          nextNextResult = f(tempResult, nextResult)
+          tempResult
+        }
+      }
+    }
   }
 }
--- src/test/scala/fib_UT.scala
+++ src/test/scala/fib_UT.scala
@@ -8,8 +8,24 @@ class Fib_UT extends FunSpec with ShouldMatchers {
       fib(0) should equal (0)
     }
 
-    it("returns 1 for 1") {
+    it("returns 1 for the first one") {
       fib(1) should equal (1)
+    }
+
+    it("returns 1 for the second one") {
+      fib(2) should equal (1)
+    }
+
+    it("returns 2 for the next one") {
+      fib(3) should equal (2)
+    }
+
+    it("returns 3 for the next one") {
+      fib(4) should equal (3)
+    }
+
+    it("returns 5 for the next one") {
+      fib(5) should equal (5)
     }
   }
 }
