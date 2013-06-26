a2700516e32efb5e2900abdc9ae5716fb441ed3b
Mario Aquino <mario.e.aquino@gmail.com>
2013-06-26 06:49:39 -0500

    Fix call to subString which takes two params.

--- src/test/scala/PartialOne_UT.scala
+++ src/test/scala/PartialOne_UT.scala
@@ -1,9 +1,9 @@
 import org.scalatest._
 import Exercise3._
 
-class PartialOneTestO extends FunSpec with ShouldMatchers {
+class PartialOneTest extends FunSpec with ShouldMatchers {
   describe("Using the return value of the partial function") {
-    partial1(1, (a:Int, b:String) => {b.substring(a)})("Mario") should equal("M")
+    partial1(1, (a:Int, b:String) => {b.substring(0, a)})("Mario") should equal("M")
   }
 
 }
