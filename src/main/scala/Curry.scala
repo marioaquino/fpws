a27ec4072a2fef9b876f27a6eb734ae33917fb54
Mario Aquino <mario.e.aquino@gmail.com>
2013-06-27 22:01:31 -0500

    Implement the curry example with several variations.

---
+++ src/main/scala/Curry.scala
@@ -0,0 +1,4 @@
+object Exercise4 {
+  def curry[A, B, C](f: (A, B) => C): A => (B => C) = (a:A) => (b:B) => f(a, b)
+}
+
---
+++ src/test/scala/Curry_UT.scala
@@ -0,0 +1,43 @@
+import org.scalatest._
+import Exercise4._
+import Exercise3._
+
+class CurryTest extends FunSpec with ShouldMatchers {
+  describe("Using the return value of a curried function") {
+    curry((a:Int, b:Int) => a * b)(2)(2) should equal(4)
+  }
+
+  describe("currying plus one application is the same as partial application") {
+    val f = (a: Int, you: String) => you.substring(0, a)
+    val curriedAndThenPartiallyApplied = curry(f)(1)
+    val partiallyApplied = partial1(1, f)
+    curriedAndThenPartiallyApplied("Mario") should equal(partiallyApplied("Mario"))
+
+  }
+
+  describe("sometimes I use a curried style") {
+    def formatLine(prefix: String)(message: String) = prefix + ": " + message
+
+    val formatter: String => String = formatLine("Simon says")
+
+    println(formatter("Jump your toes"))
+  }
+
+  describe("or we can partially apply ourselves") {
+    def formatLine(prefix: String, message: String) = prefix + ": " + message
+
+    val formatter: String => String = formatLine("Simon says", _)
+
+    println(formatter("Jump your toes"))
+  }
+
+  describe(".. which is syntax sugar for") {
+    def formatLine(prefix: String, message: String) = prefix + ": " + message
+
+    val formatter: String => String = (msg) => formatLine("Simon says", msg)
+
+    println(formatter("Jump your toes"))
+  }
+
+}
+
